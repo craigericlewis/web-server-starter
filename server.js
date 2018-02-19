5c32dbf12f4a283c11d34c873b7787bde381c6f6
craigericlewis <craiglewis1508@yahoo.co.in>
2018-02-19 14:48:12 -0500

    Add projects page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 var app = express();
 
@@ -46,6 +46,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
